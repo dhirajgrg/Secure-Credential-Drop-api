0d5a35429e3a2191be4618fe2d5941fb94d7ed3f
dhiraj <gdhiraj030@gmail.com>
2026-04-24 23:29:53 +0545

    fix package.json dependency cross-env

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(
     origin:
       process.env.NODE_ENV === "production"
         ? process.env.FRONTEND_URL
-        : "http://localhost:5173", 
+        : "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
   }),
